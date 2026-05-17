76d94a98d752c99168072e2d356a5dd985434a92
Keith Lee <fklee30680@gmail.com>
2026-05-17 16:00:38 -0400

    Allow raw file uploads up to 25 MB

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "25mb"
+    }
+  },
   reactStrictMode: true
 };
 
 export default nextConfig;
-
